8cd7b21278a21a4956fb9454b6ef0e88e6b953fb
wyy <16302010074@fudan.edu.cn>
2019-11-07 16:58:45 +0800

    添加了@RestController

--- src/main/java/com/example/littlekitchen/controller/FavoriteController.java
+++ src/main/java/com/example/littlekitchen/controller/FavoriteController.java
@@ -7,13 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@RestController
 public class FavoriteController {
     Logger logger = LoggerFactory.getLogger(getClass());
 
--- src/main/java/com/example/littlekitchen/controller/ThumbUpController.java
+++ src/main/java/com/example/littlekitchen/controller/ThumbUpController.java
@@ -6,10 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.constraints.NotNull;
 
+@RestController
 public class ThumbUpController {
     Logger logger = LoggerFactory.getLogger(getClass());
 
